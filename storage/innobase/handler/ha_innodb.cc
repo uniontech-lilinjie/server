ed648bb86e5af897f1ddc98ff2829d3efb89b642
Nikita Malyavin <nikitamalyavin@gmail.com>
2022-12-08 20:25:40 +0300

    fix main.query_cache_innodb --ps-protocol
    Commit 26399f8 adds a call to get_parent_foreign_key_list in open_tables,
    which executes twice in --ps-protocol, so a number of invalid table/db
    names reported was different.
    
    Some calls to filename_to_tablename were already silenced there, but
    calls with database name were still untouched. Now they're also silent.

--- storage/innobase/handler/ha_innodb.cc
+++ storage/innobase/handler/ha_innodb.cc
@@ -15451,7 +15451,7 @@ get_foreign_key_info(
 	memcpy(tmp_buff, foreign->referenced_table_name, len);
 	tmp_buff[len] = 0;
 
-	len = filename_to_tablename(tmp_buff, name_buff, sizeof(name_buff));
+	len = filename_to_tablename(tmp_buff, name_buff, sizeof(name_buff), 1);
 	f_key_info.referenced_db = thd_make_lex_string(
 		thd, 0, name_buff, len, 1);
 
@@ -15467,7 +15467,7 @@ get_foreign_key_info(
 	memcpy(tmp_buff, foreign->foreign_table_name, len);
 	tmp_buff[len] = 0;
 
-	len = filename_to_tablename(tmp_buff, name_buff, sizeof(name_buff));
+	len = filename_to_tablename(tmp_buff, name_buff, sizeof(name_buff), 1);
 	f_key_info.foreign_db = thd_make_lex_string(
 		thd, 0, name_buff, len, 1);
 
